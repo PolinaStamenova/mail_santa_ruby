cbf21c058248a559c34200ac876ea28b0e18284b
Polina Stamenova <polina.stamenova@gmail.com>
2024-10-22 16:54:13 +0300

    Initial commit

---
+++ lib/mail_santa_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MailSantaRuby
+  VERSION = "0.1.0"
+end
---
+++ spec/mail_santa_ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MailSantaRuby do
+  it "has a version number" do
+    expect(MailSantaRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
